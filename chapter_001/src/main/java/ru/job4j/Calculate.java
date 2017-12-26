0a5efd9f7fce6c706e2d3e0fb520f1190d3ab60a
Oleg Perov <fg12g@mail.ru>
2017-12-26 11:52:26 +0300

    1.3.JavaDoc.Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,10 +1,19 @@
 package ru.job4j;
 
+/**
+ * Class Calculate решение задачи части 1.3 урок 1.
+ * @author oleg perov
+ * @since 25.12.2017
+ * @version 1
+ */
+
+/**
+ *  Конструктор, вывод строки в консоль. 
+ *  @param arg - arg.
+ */
 public class Calculate  {
-         public static void main (String [] args){
+         public static void main (final String [] args)  {
                 System.out.println("Hello World");
                  
- }
-    
-
+         }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+*@author Oleg Perov (fg12g@mail.ru)
+*@version $ld$
+*@since 0.1
+*/
+package ru.job4j;
