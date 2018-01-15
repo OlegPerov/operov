518899b521d691ddbf85726f1bb9c9b6a8f51b91
Oleg Perov <fg12g@mail.ru>
2018-01-15 21:01:18 +0300

    3.2. Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,16 @@
+﻿package ru.job4j.max;
+
+/**
+ * @author Oleg Perov(fg12g@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Принимает 2 значения ( int first, int second)
+     * @return возвращает максимальное число
+     */
+    public int max(int first, int second) {
+          return first > second ? first: second;
+    }
+}
